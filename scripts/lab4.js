c7bda5b684ecb97106cceb97562d2db77bd29b5a
Jason Huang <156235775+jasonhuan9@users.noreply.github.com>
2025-04-29 23:04:02 -0700

    fix bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return false;
+    }
     if (add) {
         let result = 0;
 
@@ -32,6 +35,9 @@ function discountPrices(prices, discount) {
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
+        if (typeof prices[i] !== 'number') {
+            return false;
+        }
         discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
