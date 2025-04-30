e8091c91357e074fbf4f805ae31bd7a8ab524033
Jason Huang <156235775+jasonhuan9@users.noreply.github.com>
2025-04-29 22:47:47 -0700

    fix bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -24,12 +24,15 @@ function sumValues(num1, num2, add) {
  * @param {*} discount, a number between 0-1 to represent the discount. 
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
-function discountPrices(prices, discount) {
+function discountPrices(prices, discount) { 
+    if (prices.length == 0) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
